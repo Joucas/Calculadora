21fac28791c466d80a0315caf1f07c9ebbf8ceba
Joucas <65504436+Joucas@users.noreply.github.com>
2023-06-27 05:35:42 +0000

    Finalizando o projeto Calculadora

--- calculadora/src/app/home/home.page.ts
+++ calculadora/src/app/home/home.page.ts
@@ -7,7 +7,42 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // variável que recebe os valores dos botões
+  valor = '';
 
-  constructor() {}
+  // função para capturar o valor do botão
+  acumularValores(valor_btn : string){
+    this.valor += valor_btn;
+  }
 
+  // função para calcular os valores dados pelo usuário
+  // eval() serve para transformar uma combinação de caracteres, como exemplo "10 - 25" em uma linha de código executavel pelo JS
+  calcularValor(){
+    const res = eval(this.valor);
+    this.valor = res.toString();
+  }
+
+  // função para limpar a calculadora, transformando a variavel do valor salvo como nada
+  clear(){
+    this.valor = '';
+  }
+
+  // função para apagar a última coisa digitada
+  apagarUltimo(){
+    const ultimo_valor = this.valor.slice(0,-1)
+    this.valor = ultimo_valor;
+  }
+
+  // função para calcular porcentagem
+  calcularPorcentagem(){
+    const porcentagem = eval(this.valor) / 100;
+    this.valor = porcentagem.toString();
+    this.valor += '*';
+  }
+
+  // função para inverter o sinal do valor adicionado na calculadora
+  inverterSinal(){
+    const valor_invertido = eval(this.valor) * (-1);
+    this.valor = valor_invertido.toString();
+  }
 }
